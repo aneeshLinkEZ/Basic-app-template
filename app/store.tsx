1fa3482cafdef75f7a1d700c1926b71cd3d1b680
= <aneesh@linkeztech.com>
2022-09-30 15:15:40 +0530

    first commit

---
+++ app/store.tsx
@@ -0,0 +1,27 @@
+import { combineReducers, configureStore } from '@reduxjs/toolkit'
+import counderReducer from './slices/counter/counter'
+import AsyncStorage from '@react-native-async-storage/async-storage'
+import { persistStore, persistReducer } from 'redux-persist'
+
+const persistConfig = {
+    key: 'root',
+    storage: AsyncStorage,
+};
+
+const rootReducer = combineReducers({
+    counder: counderReducer,
+});
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+export const store = configureStore({
+    reducer: persistedReducer,
+
+})
+
+export let persistor = persistStore(store);
+
+// Infer the `RootState` and `AppDispatch` types from the store itself
+export type RootState = ReturnType<typeof store.getState>
+// Inferred type: {posts: PostsState, comments: CommentsState, users: UsersState}
+export type AppDispatch = typeof store.dispatch
